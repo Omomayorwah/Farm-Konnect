68b2b8cd9ae8ab9dc46d62945beabdfdf6f85414
Omomayorwah <kintunde2010@gmail.com>
2025-11-12 03:40:27 +0100

    -m

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin:'https://farm-konnect-delta.vercel.app',
+  origin:'https://farm-konnect-three.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
